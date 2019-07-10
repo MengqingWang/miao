840105035bf6d383b8faea54303506651b1db68e
MengqingWang <maryspro@TS1989-MacBook-Pro.local>
2019-07-10 17:22:27 +0800

    first commit

---
+++ lodash/MengqingWang-lodash.js
@@ -0,0 +1,16 @@
+var MengqingWang = {
+    
+    findIndex:function(array, callback) {
+        let index = -1
+        let l = array.length
+        for (let i = 0; i < l; i++) {
+            let s = array[i]
+            if (callback(s)) {
+                index = i
+                break
+            }
+        }
+        return index
+    }
+
+}
