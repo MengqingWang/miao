9a97ccd07831198bbe54fbee9eef0ba699ad5dcc
MengqingWang <maryspro@TS1989-MacBook-Pro.local>
2019-07-15 10:52:16 +0800

    try try

--- lodash/mengqingwang-lodash.js
+++ lodash/mengqingwang-lodash.js
@@ -1,16 +1,62 @@
-var mengqingwang = {
+// var mengqingwang = {
+//
+//     findIndex:function(array, callback) {
+//         let index = -1
+//         let l = array.length
+//         for (let i = 0; i < l; i++) {
+//             let s = array[i]
+//             if (callback(s)) {
+//                 index = i
+//                 break
+//             }
+//         }
+//         return index
+//     }
+//
+// }
+var mengqingwang = (function () {
 
-    findIndex:function(array, callback) {
-        let index = -1
-        let l = array.length
-        for (let i = 0; i < l; i++) {
-            let s = array[i]
-            if (callback(s)) {
-                index = i
-                break
+function iteratee(action) {
+    if (typeof action === 'string') {
+        return property(action)
+    } else if (Array.isArray(action)) {
+        return matches(action)
+    } else if (typeof action === 'object') {
+        return matchProperty(action)
+    } else return action
+}
+
+function property(str) {
+    return function(obj) {
+        return obj[str]
+    }
+}
+
+function matches(arr) {
+    return function (obj) {
+        return obj[arr[0]] === arr[1]
+    }
+}
+
+function matchProperty(m) {
+    return function (obj) {
+        for (let key in m) {
+            if (obj[key] != m[key]) {
+                return false
             }
         }
-        return index
+        return true
     }
-
 }
+
+function findIndex(array, predicate, fromIndex = 0) {
+    predicate = iteratee(predicate)
+    for (var i = fromIndex; i < array.length; i++) {
+        if (predicate(array[i])) {
+            return i
+        }
+    }
+    return -1
+}
+
+})()
---
+++ lodash/xx.js
@@ -0,0 +1,31 @@
+function baseFindIndex(array, predicate, fromIndex, fromRight) {
+  var length = array.length,
+      index = fromIndex + (fromRight ? 1 : -1);
+
+  while ((fromRight ? index-- : ++index < length)) {
+    if (predicate(array[index], index, array)) {
+      return index;
+    }
+  }
+  return -1;
+}
+
+function findIndex(array, predicate, fromIndex) {
+  var length = array == null ? 0 : array.length;
+  if (!length) {
+    return -1;
+  }
+  var index = fromIndex == null ? 0 : toInteger(fromIndex);
+  if (index < 0) {
+    index = nativeMax(length + index, 0);
+  }
+  return baseFindIndex(array, getIteratee(predicate, 3), index);
+}
+
+var users = [
+  { 'user': 'barney',  'active': false },
+
+  { 'user': 'fred',    'active': false },
+
+  { 'user': 'pebbles', 'active': true }
+];
