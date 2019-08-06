8d4cbbd617b3a768b84193a29d2e35ed0189c6b6
MengqingWang <maryspro@TS1989-MacBook-Pro.local>
2019-08-06 15:56:01 +0800

    0806 r99

--- lodash/mengqingwang-lodash.js
+++ lodash/mengqingwang-lodash.js
@@ -1,5 +1,6 @@
 var mengqingwang = function() {
 
+//常用的迭代器函数
 function iteratee(argument) {
     if (Object.prototype.toString.call(argument) === "[object String]") {
         return function (object) {
@@ -115,6 +116,34 @@ function dropRight(array, n = 1) {
     return array.slice(0, array.length - n)
 }
 
+//返回一个新的数组  数组起点从predicate返回flase的那个值开始一直到最后
+function dropWhile(array, predicate) {
+    predicate = iteratee(predicate)
+    var index = 0
+    for (var i = 0; i < array.length; i++) {
+        if (!predicate(array[i])) {
+            index = i
+            break
+        }
+    }
+    return array.slice(i)
+}
+
+//返回一个新的数组  数组起点从predicate返回flase的那个值开始一直往前
+function dropRightWhile(array, predicate) {
+    predicate = iteratee(predicate)
+    var index = 0
+    for (var i = array.length - 1; i >= 0; i--) {
+        console.log(i , predicate(array[i]))
+        if (!predicate(array[i])) {
+            index = i
+            break
+        }
+    }
+    console.log(index)
+    return array.slice(0, index + 1)
+}
+
 function fill(array, value, start = 0, end = array.length) {
     for (let i = start; i < end; i++) {
         array[i] = value
@@ -215,10 +244,27 @@ function initial(array) {
     return array
 }
 
-// function intersection(arrays) {
-//     //有点烦这个   先空着
-// }
-
+function intersection(...arrays) {
+    //并没有考虑有个数在某个数组里面出现了两次  而在某个数组里没有出现的情况  暂时先这样吧
+    var arr = [].concat(...arrays)
+    console.log(arr)
+    var unique = uniq(arr) //这里不能用new Set   因为Set返回的是一个对象   不是数组
+    //console.log(unique, typeof unique)
+    var count = Array(unique.length).fill(0) //计数器数组 跟去重数组的下标一一对应 表示的是unique数组中每个数字出现的次数
+    var result = []
+    var l = arrays.length
+    for (var i = 0; i < arr.length; i++) {
+        var index = findIn(unique, arr[i])
+        count[index] += 1
+    }
+    console.log(count)
+    for (var i = 0; i < count.length; i++) {
+        if (count[i] == l) {
+            result.push(unique[i])
+        } //返回出现了l次的数字   也就是在每个小数组里面都出现
+    }
+    return result
+}
 
 function join(array, separator=',') {
     var s = array[0]
@@ -365,6 +411,24 @@ function uniq(array) {
     return union(array)
 }
 
+function unionBy(...args) {
+    var predicate = iteratee(args[args.length - 1])
+    var arrays = args.slice(0, args.length - 1)
+    var result = []
+    var arr = [].concat(...arrays)
+    arr.forEach(val => {
+        if (!result.map(it => predicate(it)).includes(predicate(val))) {
+            result.push(val)
+        }
+    })
+    return result
+}
+
+function uniqBy(array, predicate) {
+    var args = [array, predicate]
+    return unionBy(...args)
+}
+
 // function without(array, ...values) {
 //     var result = []
 //     for (var i = 0; i < array.length; i++) {
@@ -415,20 +479,26 @@ function xor(...arrays) {
     return result
 }
 
-// function zip(...arrays) {
-//     // 暂时不考虑各个数组里面元素个数不等的情况   也就是各个数组里面元素的个数相等
-//     var result = []
-//     var l = arrays[0].length
-//     for (var i = 0; i < l; i++) {
-//         result.push  //需要双重循环 不写下去了
-//
-// }
-
 //arrays有三个小数组   每个小数组有两个元素
 //这个好像就是力扣的867矩阵转置啊
 function zip(...arrays) {
-  return arrays[0].map((_,index)=>arrays.map(row=>row[index]))
+    return arrays[0].map((_, idx) => {
+        return arrays.map(row => row[idx])
+    })
 }
+
+function unzip(arrays) {
+    return arrays[0].map((_, idx) => {
+        return arrays.map(row => row[idx])
+    })
+} //跟zip唯一的区别就是传参的时候 没有点点点
+
+//区别就是一个zip传参的时候是(['fred', 'barney'], [30, 40], [true, false])   三个数组 三个数组看成一个整体就是...arrays
+//而unzip传参传的是zip的结果  也就是 ([['fred', 30, true], ['barney', 40, false]])这本身就是一个大数组  所以传参的时候是arrays
+
+var zipped = zip(['fred', 'barney'], [30, 40], [true, false])
+console.log(zipped) //[['fred', 30, true], ['barney', 40, false]]
+console.log(unzip(zipped)) //[['fred', 'barney'], [30, 40], [true, false]]
 
 function zipObject(props = [], values = []) {
     var map = {}
@@ -438,18 +508,38 @@ function zipObject(props = [], values = []) {
     return map;
 }
 
+function countBy(collection, predicate = identity) {
+    var map = {}
+    predicate = iteratee(predicate)
+    collection = collection.map(it => predicate(it))
+    for (var i = 0; i < collection.length; i++) {
+        //console.log(predicate(collection[i]))
+        if (collection[i] in map) {
+            map[collection[i]]++
+        } else {
+            map[collection[i]] = 1
+        }
+    }
+    return map
+}
+// countBy([6.1, 4.2, 6.3], Math.floor)
+// countBy(['one', 'two', 'three'], 'length')
+// 举个例子
+// var a = 'one'
+// a["length"]
+//输出3
 
-// function filter(ary,predicate) {
-//     //这句话没用
-//     // predicate = iteratee(predicate)
-//     var result = []
-//     for (var i = 0; i < ary.length; i++) {
-//         if (predicate(ary[i], i , ary)) {
-//             result.push(ary[i])
-//         }
-//     return result
-//     }
-// }
+function filter(ary,predicate) {
+    //这句话没用
+    // predicate = iteratee(predicate)
+    var result = []
+    for (var i = 0; i < ary.length; i++) {
+        if (predicate(ary[i], i , ary)) {
+            result.push(ary[i])
+        }
+    return result
+    }
+}
 
 function every(ary, predicate) {
     //这句话有用   虽然我也不知道是什么意思
@@ -571,6 +661,8 @@ return {
     differenceBy,
     drop,
     dropRight,
+    dropWhile,
+    dropRightWhile,
     fill,
     findIndex,
     findLastIndex,
@@ -581,7 +673,7 @@ return {
     head,
     indexOf,
     initial,
-    // intersection,
+    intersection,
     join,
     last,
     lastIndexOf,
@@ -599,11 +691,15 @@ return {
     takeRight,
     union,
     uniq,
+    unionBy,
+    uniqBy,
     without,
     xor,
     zip,
+    unzip,
     zipObject,
-    // filter,
+    countBy,
+    filter,
     every,
     some,
     negate,
--- lodash/xxx.js
+++ lodash/xxx.js
@@ -196,7 +196,7 @@ function differenceBy(array, ...values) {
         newvalues = newvalues.concat(values[i])
     }
     newvalues = newvalues.map(it => func(it))
-    console.log(newvalues)
+    //console.log(newvalues)
     for (var i = 0; i < array.length; i++) {
         if(findIn(newvalues, func(array[i])) == -1) {
             result.push(array[i])
@@ -205,4 +205,149 @@ function differenceBy(array, ...values) {
     return result
 }
 
-console.log(differenceBy([{ 'x': 2 }, { 'x': 1 }], [{ 'x': 1 }], 'x'))
+//console.log(differenceBy([{ 'x': 2 }, { 'x': 1 }], [{ 'x': 1 }], 'x'))
+
+function dropWhile(array, predicate) {
+    predicate = iteratee(predicate)
+    var index = 0
+    for (var i = 0; i < array.length; i++) {
+        if (!predicate(array[i])) {
+            index = i
+            break
+        }
+    }
+    return array.slice(i)
+}
+
+function dropRightWhile(array, predicate) {
+    predicate = iteratee(predicate)
+    var index = 0
+    for (var i = array.length - 1; i >= 0; i--) {
+        //console.log(i , predicate(array[i]))
+        if (!predicate(array[i])) {
+            index = i
+            break
+        }
+    }
+    console.log(index)
+    return array.slice(0, index + 1)
+}
+
+function union(...array) {
+    var result = []
+    var newarr = [].concat(...array)
+    newarr.forEach(val => {
+        if (!result.includes(val)) {
+            result.push(val)
+        }
+    })
+    return result
+}
+
+function uniq(array) {
+    return union(array)
+}
+
+function intersection(...arrays) {
+    //并没有考虑有个数在某个数组里面出现了两次  而在某个数组里没有出现的情况  暂时先这样吧
+    var arr = [].concat(...arrays)
+    //console.log(arr)
+    var unique = uniq(arr) //这里不能用new Set   因为Set返回的是一个对象   不是数组
+    var count = Array(unique.length).fill(0) //计数器数组 跟去重数组的下标一一对应 表示的是unique数组中每个数字出现的次数
+    var result = []
+    var l = arrays.length
+    for (var i = 0; i < arr.length; i++) {
+        var index = findIn(unique, arr[i])
+        count[index] += 1
+    }
+    //console.log(count)
+    for (var i = 0; i < count.length; i++) {
+        if (count[i] == l) {
+            result.push(unique[i])
+        } //返回出现了l次的数字   也就是在每个小数组里面都出现
+    }
+    return result
+}
+
+//console.log(intersection([2, 1], [2, 3]))
+
+function unionBy(...args) {
+    var predicate = iteratee(args[args.length - 1])
+    var arrays = args.slice(0, args.length - 1)
+    var result = []
+    var arr = [].concat(...arrays)
+    arr.forEach(val => {
+        if (!result.map(it => predicate(it)).includes(predicate(val))) {
+            result.push(val)
+        }
+    })
+    return result
+}
+
+//console.log(unionBy([2.1], [1.2, 2.3], Math.floor))
+
+//console.log(unionBy([{ 'x': 1 }], [{ 'x': 2 }, { 'x': 1 }], 'x'))
+
+
+// function zip(...arrays) {
+//     // 暂时不考虑各个数组里面元素个数不等的情况   也就是各个数组里面元素的个数相等
+//     var result = []
+//     var l = arrays[0].length
+//     for (var i = 0; i < l; i++) {
+//         result.push  //需要双重循环 不写下去了
+// }
+
+//arrays有三个小数组   每个小数组有两个元素
+//这个好像就是力扣的867矩阵转置啊
+function zip(...arrays) {
+    return arrays[0].map((_, idx) => {
+        return arrays.map(row => row[idx])
+    })
+}
+
+function unzip(arrays) {
+    return arrays[0].map((_, idx) => {
+        return arrays.map(row => row[idx])
+    })
+} //跟zip唯一的区别就是传参的时候 没有点点点
+
+
+//区别就是一个zip传参的时候是(['fred', 'barney'], [30, 40], [true, false])   三个数组 三个数组看成一个整体就是...arrays
+//而unzip传参传的是zip的结果  也就是 ([['fred', 30, true], ['barney', 40, false]])这本身就是一个大数组  所以传参的时候是arrays
+
+var zipped = zip(['fred', 'barney'], [30, 40], [true, false])
+//console.log(zipped) //[['fred', 30, true], ['barney', 40, false]]
+//console.log(unzip(zipped)) //[['fred', 'barney'], [30, 40], [true, false]]
+
+function unionBy(...args) {
+    var predicate = iteratee(args[args.length - 1])
+    var arrays = args.slice(0, args.length - 1)
+    var result = []
+    var arr = [].concat(...arrays)
+    arr.forEach(val => {
+        if (!result.map(it => predicate(it)).includes(predicate(val))) {
+            result.push(val)
+        }
+    })
+    return result
+}
+
+function uniqBy(array, predicate) {
+    var args = [array, predicate]
+    return unionBy(...args)
+}
+
+//console.log(uniqBy([2.1, 1.2, 2.3], Math.floor))
+//console.log(uniqBy([{ 'x': 1 }, { 'x': 2 }, { 'x': 1 }], 'x'))
+
+function filter(ary,predicate) {
+    //这句话没用
+    // predicate = iteratee(predicate)
+    var result = []
+    for (var i = 0; i < ary.length; i++) {
+        if (predicate(ary[i], i , ary)) {
+            result.push(ary[i])
+        }
+    return result
+    }
+}
